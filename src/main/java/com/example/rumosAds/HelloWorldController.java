69ab2d580fc725b28c2a56f5ba3179f265578051
drekerd <marioalves.silva17@gmail.com>
2019-07-15 21:46:33 +0100

    dynamicMessage

--- src/main/java/com/example/rumosAds/HelloWorldController.java
+++ src/main/java/com/example/rumosAds/HelloWorldController.java
@@ -1,13 +1,16 @@
 package com.example.rumosAds;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/test")
-    public String testHello(){
+    @RequestMapping("/cenas")
+    public String testHello(Model model){
+        String name = "mario";
+        model.addAttribute("cenas", name);
         return "helloTest";
     }
 }
